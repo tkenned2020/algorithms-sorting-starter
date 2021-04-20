42094babc44d95d9619877f3be37f6869096d4d0
Alejandro Ventura De Moya <alexventura@gmail.com>
2021-04-20 14:57:46 -0500

    work on insertion sort

--- problems/03-insertion-sort.js
+++ problems/03-insertion-sort.js
@@ -1,8 +1,42 @@
 // Implement Insertion Sort
 
-function insertionSort(list) {
-  // your code here
+function swap(arr, index1, index2) {
+  let temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
 }
+
+function insertionSort(array) {
+  // your code here
+  for (let index = 1; index < array.length; index++) {
+    let value = array[index];
+    for (let j = 0; j < index; j--) {
+      if (array[j] < value) {
+        swap(array, array[index], array[j]);
+      }
+    }
+  }
+  return array;
+}
+
+
+//function selectionSort(list) {
+//  // your code here
+//
+//  for (let j = 0; j < list.length; j++) {
+//    let min = j;
+//    for (let index = j + 1; index < list.length; index++) {
+//      if (list[min] > list[index]) {
+//        min = index;
+//      };
+//    };
+//    swap(list, j, min);
+//  };
+//  return list;
+//};
+
+
+
 
 module.exports = {
   insertionSort
