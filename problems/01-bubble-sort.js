1562362b8ab6cfabfe040021dca1f3244154e36b
tkenned2020 <tkenned2020@outlook.com>
2021-04-20 14:54:07 -0400

    prb 1

--- problems/01-bubble-sort.js
+++ problems/01-bubble-sort.js
@@ -10,14 +10,13 @@ function swap(array, idx1, idx2) {
 function bubbleSort(array) {
   // your code here
   for (let index = 0; index < array.length; index++) {
-    let pointer = 0
+    let pointer = 0;
     if (array[index] > array[index + 1]) {
       swap(array, array[index], array[index + 1]);
-      
-    };
-  };
+    }
+  }
   return array;
-};
+}
 
 let array1 = [2, -1, 4, 3, 7, 3];
 console.log(bubbleSort(array1));
