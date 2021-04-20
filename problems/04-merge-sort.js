6e2f124bb51f6fe1df4259c5b8846c2ed2a1d8a3
Alejandro Ventura De Moya <alexventura@gmail.com>
2021-04-20 15:45:12 -0500

    work on merge sort

--- problems/04-merge-sort.js
+++ problems/04-merge-sort.js
@@ -3,22 +3,30 @@
 function merge(array1, array2) {
   // your code here
   let result = [];
-  while((array1.length > 0) || (array2.length > 0)) {
-    for(let i = 0; ){
-     if(array1[0] < array2[0]){
-      eleRemoved = array1.shift(array1[0])
-        result.push(eleRemoved/*array1[0]*/);
-      } else if(array2[0] < array1[0]){
-        eleRemoved1 = array2.shift(array2[0])
-        result.push(eleRemoved1/*array2[0]*/);
-      };
+  while ((array1.length > 0) || (array2.length > 0)) {
+    if (array1[0] < array2[0]) {
+      eleRemoved = array1.shift(array1[0]);
+      result.push(eleRemoved);
+    } else if (array2[0] < array1[0]) {
+      eleRemoved1 = array2.shift(array2[0])
+      result.push(eleRemoved1);
     };
+
   };
-};
+  return result.concat(array1).concat(array2);
+}
 
 function mergeSort(array) {
   // your code here
+  let middle = Math.floor(array.length / 2);
+  let leftHalf = array.slice(0, middle);
+  let rightHalf = array.slice(middle);
+
+  // mergeSort(leftHalf);
+  // mergeSort(rightHalf);
+  return merge(leftHalf, rightHalf);
 }
+
 
 module.exports = {
   merge,
