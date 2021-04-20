c1883b3fe962a7c45970a840630f14fb73120ac8
tkenned2020 <tkenned2020@outlook.com>
2021-04-20 14:11:42 -0400

    start problem 1

--- problems/01-bubble-sort.js
+++ problems/01-bubble-sort.js
@@ -9,9 +9,20 @@ function swap(array, idx1, idx2) {
 
 function bubbleSort(array) {
   // your code here
-}
+  for (let index = 0; index < array.length; index++) {
+    let pointer = 0
+    if (array[index] > array[index + 1]) {
+      swap(array, array[index], array[index + 1]);
+      
+    };
+  };
+  return array;
+};
 
+let array1 = [2, -1, 4, 3, 7, 3];
+console.log(bubbleSort(array1));
+// result [ 2, -1, undefined, undefined, 7, 3, <1 empty item>, 3, '-1': 4 ]
 module.exports = {
   bubbleSort,
-  swap
+  swap,
 };
