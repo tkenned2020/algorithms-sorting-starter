86af4301e994f69a83ee8bb3f2df15c617a0cea0
tkenned2020 <tkenned2020@outlook.com>
2021-04-20 17:21:56 -0400

    working on prob 2

--- problems/04-merge-sort.js
+++ problems/04-merge-sort.js
@@ -2,7 +2,19 @@
 
 function merge(array1, array2) {
   // your code here
-}
+  let result = [];
+  while((array1.length > 0) || (array2.length > 0)) {
+    for(let i = 0; ){
+     if(array1[0] < array2[0]){
+      eleRemoved = array1.shift(array1[0])
+        result.push(eleRemoved/*array1[0]*/);
+      } else if(array2[0] < array1[0]){
+        eleRemoved1 = array2.shift(array2[0])
+        result.push(eleRemoved1/*array2[0]*/);
+      };
+    };
+  };
+};
 
 function mergeSort(array) {
   // your code here
