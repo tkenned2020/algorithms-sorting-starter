8141b004b6349971e0cf4f5e35d6ad12d38e7d69
tkenned2020 <tkenned2020@outlook.com>
2021-04-20 18:35:34 -0400

    complete problem 5

--- problems/04-merge-sort.js
+++ problems/04-merge-sort.js
@@ -3,32 +3,34 @@
 function merge(array1, array2) {
   // your code here
   let result = [];
-  while ((array1.length > 0) || (array2.length > 0)) {
+  while (array1.length || array2.length) {
     if (array1[0] < array2[0]) {
       eleRemoved = array1.shift(array1[0]);
       result.push(eleRemoved);
     } else if (array2[0] < array1[0]) {
-      eleRemoved1 = array2.shift(array2[0])
+      eleRemoved1 = array2.shift(array2[0]);
       result.push(eleRemoved1);
-    };
-
-  };
+    }
+  }
   return result.concat(array1).concat(array2);
 }
 
-function mergeSort(array) {
+
+ function mergeSort(array) {
   // your code here
   let middle = Math.floor(array.length / 2);
+  if (array.length <= 1) {
+    return array;
+  }
   let leftHalf = array.slice(0, middle);
   let rightHalf = array.slice(middle);
 
   // mergeSort(leftHalf);
   // mergeSort(rightHalf);
   return merge(leftHalf, rightHalf);
-}
-
+ }
 
 module.exports = {
   merge,
-  mergeSort
+  mergeSort,
 };
--- problems/05-quick-sort.js
+++ problems/05-quick-sort.js
@@ -2,9 +2,27 @@
 
 function quickSort(array) {
   // your code here
-}
+  if(array.length === 0){
+    return array;
+  }
 
+  let pivot = array.shift(array[0]);
+  let left = [];
+  let right = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < pivot) {
+      left.push(array[i]);
+    } else
+    {right.push(array[i])}
+  };
+
+  let leftSort = quickSort(left)
+  let rightSort = quickSort(right)
+
+  return leftSort.concat([pivot]).concat(rightSort)
+};
 
 module.exports = {
-  quickSort
+  quickSort,
 };
